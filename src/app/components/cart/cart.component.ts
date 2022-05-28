15f817944e88b869010614d0c9211dc68b7dd3bc
D3konR3kon <dilavuyo@outlook.com>
2022-05-28 05:02:10 +0200

    6

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -17,48 +17,69 @@ export class CartComponent implements OnInit {
 
   ngOnInit(): void {
     this.getItems()
-    this.getTotal();
+    this.Total();
   }
   getItems(){
     this.items = this.cartService.getItems();
   }
   //delete item
+  // deletei(id: any): void {
+  //   let index = this.items.findIndex((item: { product_id: any; }) => item.product_id === id);
+  //   this.items.splice(index, 1);
+  //   this.Total();
+  
+  // }
   deletei(i: number): void {
     this.items.splice(i, 1);
+    this.Total();
   
   }
-
-  //adding total
-    // total(){
-    //   this.items.map( (elem: any) => {
-    //     if(elem.quantity) 
-    //       this.totalAmount += elem.price*elem.quantity
+  
+  Total() {
+    this.totalAmount = 0
+    this.items.forEach((item: { quantity: number; price: number; }) => {
+      this.totalAmount += (item.quantity * item.price)
+    })
+  }
+  // //adding total
+  //   // total(){
+  //   //   this.items.map( (elem: any) => {
+  //   //     if(elem.quantity) 
+  //   //       this.totalAmount += elem.price*elem.quantity
         
-    //   })
+  //   //   })
+  //   }
+    // getTotal(){
+    //   let total = 0;
+    //   let price = 0;
+    //   let quantity= 0;
+    //   for (var i = 0; i < this.items.length; i++) {
+    //       if (this.items[i].price) {
+    //         price += this.items[i].price;
+    //         quantity += this.items[i].quantity;
+            
+    //           total += price*quantity
+    //          this.totalAmount = total;
+    //       }else if(!this.items[i].price){
+    //         total = 0;
+    //         this.totalAmount = total;
+    //       }
+    //   }
+    //   return total;
     // }
-    getTotal(){
-      let total = 0;
-      for (var i = 0; i < this.items.length; i++) {
-          if (this.items[i].price) {
-              total += this.items[i].price * this.items[i].quantity;
-              this.totalAmount = total;
-          }
-      }
-      return total;
-    }
+    
   
-  
-    increment(quantity: any, index: number){
-      quantity++
-      this.items[index].quantity = quantity
-      this.total()
-    }
-    decrement(quantity: any, index: number){
-      quantity--
-      this.items[index].quantity = quantity
-      this.total()
-    }}
+    // increment(quantity: any, index: number){
+    //   quantity++
+    //   this.items[index].quantity = quantity
+    //   this.total()
+    // }
+    // decrement(quantity: any, index: number){
+    //   quantity--
+    //   this.items[index].quantity = quantity
+    //   this.total()
+    // }}
  
-
+  }
 
 
